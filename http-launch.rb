4ce1bd88375da8ec8f552628b2f81423a6ec864f
tyabuta <gmforip@gmail.com>
2013-05-01 12:59:27 +0900

    new file:   http-launch.rb
    WEBrickを使用したHTTPローカルサーバーをたてる。

---
+++ http-launch.rb
@@ -0,0 +1,17 @@
+#!/usr/bin/env ruby
+#####################################################################
+#
+#          カレントディレクトリにWEBrickを起動させる。
+#
+#                                                  (c) 2013 tyabuta.
+#####################################################################
+require 'webrick'
+include WEBrick
+
+server = HTTPServer.new(
+:Port         => 8000,
+:DocumentRoot => Dir::pwd
+)
+trap("INT"){ server.shutdown }
+server.start
+

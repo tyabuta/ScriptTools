f4691d15af27c1b1f37b82283252c9f2c23663b3
tyabuta <gmforip@gmail.com>
2013-05-13 09:39:46 +0900

    "関数名修正"

--- git-push.rb
+++ git-push.rb
@@ -12,7 +12,7 @@
 # カレントブランチを取得する。
 # 取得に失敗した場合は空文字列を返す。
 #
-def GitBrantchGetCurrent()
+def GitBranchGetCurrent()
     git_status = `git status 2>&1`
     if git_status =~ /# On branch (.+)$/ then
         current_branch = $1
@@ -25,7 +25,7 @@ end
 # __Main__
 
 # カレントブランチの取得
-current_branch = GitBrantchGetCurrent()
+current_branch = GitBranchGetCurrent()
 if "" == current_branch then
     puts "カレントブランチの取得に失敗しました。"
     exit 1

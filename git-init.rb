e975dab4a5cde12fc87a1ea78e0389a21aa3faf2
tyabuta <gmforip@gmail.com>
2013-06-08 00:22:08 +0900

    add git-init.rb
    Gitリポジトリの初期化スクリプトを追加

---
+++ git-init.rb
@@ -0,0 +1,72 @@
+#!/usr/bin/env ruby
+# ********************************************************************
+#
+#                 Gitリポジトリの初期化スクリプト
+#
+#                                                   (c) 2013 tyabuta.
+# ********************************************************************
+
+# __Func__
+
+#
+# 指定ファイルに文字列を追記する。
+#
+def FileWriteAppend(filename, str)
+    open(filename, "a") do |f|
+        f.write str
+        return true
+    end
+    return false
+end
+
+#
+# Data構造処理用の関数
+#
+def AppendData(data)
+    ret = FileWriteAppend(data[:filename], data[:text])
+    if ret then
+        puts data[:message]
+    end
+end
+
+
+
+# __Main__
+
+DATA = [
+{ :filename => ".gitignore",
+   :message => "[write] .gitignore",
+      :text => <<"EOF"
+.DS_Store
+project.xcworkspace/
+xcuserdata/
+EOF
+},
+
+{ :filename => ".gitattributes",
+   :message => "[write] .gitattributes",
+      :text => <<"EOF"
+*.ai binary
+*.xib bi
+!*.pbxproj binary
+EOF
+},
+] # End of DATA
+
+
+
+# git init --------------------------------------
+ret = system("git init")
+if ret then
+    puts "[init] git repository"
+else
+    abort
+end
+
+# file apppend ----------------------------------
+DATA.each do |data|
+    AppendData data
+end
+
+exit 0
+

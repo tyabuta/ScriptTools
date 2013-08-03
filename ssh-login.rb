6daf6826a937e29601a6469d851c5d23f124997e
tyabuta <gmforip@gmail.com>
2013-08-03 22:02:35 +0900

    設定ファイルの雛形出力機能を追加

--- ssh-login.rb
+++ ssh-login.rb
@@ -8,23 +8,30 @@
 #                                                  (c) 2013 tyabuta.
 #####################################################################
 
-#
-# ~/.sshlogin.yml の設定例
-# -----------------------------------------------
-# - server: ServerName1
-#   command: ssh User@host1 -p 20
-#
-# - server: ServerName2
-#   command: ssh User@host2 -p 20
-# -----------------------------------------------
-#
-
-
+require 'optparse'
 require 'yaml'
 
 
+# --- Const ---
 
-# __Func__
+# バージョン
+Version = '1.1.0'
+
+# 設定ファイル読み込みのPATH
+CONF_PATH = '~/.sshlogin.yml'
+
+# 設定ファイルのテンプレート
+CONF_TEMPLATE = <<"EOF"
+-  server: ServerName1
+  command: ssh User@host1 -p 20
+-  server: ServerName2
+  command: ssh User@host2 -p 20 -i ~/.ssh/id_rsa
+EOF
+
+
+
+
+# --- Func ---
 
 # -------------------------------------------------------------------
 # プロンプトによる項目の選択関数
@@ -61,7 +68,7 @@ end
 
 
 # -------------------------------------------------------------------
-# ハッシュ配列から、指定キーの値から配列を作成する。
+# ハッシュ配列から、指定キーの値で配列を作成する。
 # -------------------------------------------------------------------
 def ArrayMakeWithHashKey(hash_array, key)
     arr = []
@@ -69,13 +76,53 @@ def ArrayMakeWithHashKey(hash_array, key)
     return arr
 end
 
+# -------------------------------------------------------------------
+# ヘルプメッセージ出力用の関数
+# -------------------------------------------------------------------
+def PrintHelp()
+    puts <<"EOF"
+ホストサーバー接続用のスクリプト
+コマンドを起動すると接続するホスト先を選択できます。
+※設定ファイルを作成しておく必要があります。
+
+設定ファイルを作成するには下記コマンドを使用する事で雛形を作成できます。
+#{File.basename($0)} -o > #{CONF_PATH}
+
+#{CONF_PATH} の設定例
+#{'-' * 50}
+#{CONF_TEMPLATE.chomp}
+#{'-' * 50}
+EOF
+end
+
+
+# -------------------------------------------------------------------
+# オプション解析の関数
+# -------------------------------------------------------------------
+def OptionParse()
+    opt = OptionParser.new
+    opt.on('-h', '--help') {
+        PrintHelp()
+        exit 0
+    }
+    opt.on('-o', '--output-conf') {
+        puts CONF_TEMPLATE
+        exit 0
+    }
+    opt.parse!(ARGV)
+end
 
 
 
-# __Main__
+
+# --- Main ---
+
+# オプション解析
+OptionParse()
+
 
 # YAMLファイルの存在確認
-yaml_file_path = ENV['HOME'] + '/.sshlogin.yml'
+yaml_file_path = File.expand_path(CONF_PATH)
 if false == File.exist?(yaml_file_path) then
     puts  yaml_file_path + ' ファイルが見つかりません。'
     exit -1

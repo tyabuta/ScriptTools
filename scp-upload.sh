6e281f27ae56054a99e8b20009159c4d179d571d
tyabuta <gmforip@gmail.com>
2014-04-30 11:40:14 +0900

    add scp-upload.sh

--- scp-upload.sh
+++ scp-upload.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/env bash
 
-
 # 指定ファイルPATH
 file=$1
 if [ -z "$file" ]; then
@@ -18,11 +17,17 @@ fi
 # 設定読み込み
 rsaKey=`ruby -r yaml -e "puts YAML.load(open('$yamlPath').read)['rsa-key']"`
 host=`ruby -r yaml -e "puts YAML.load(open('$yamlPath').read)['host']"`
+remoteDir=`ruby -r yaml -e "puts YAML.load(open('$yamlPath').read)['remote-dir']"`
 
-# download
-if [ "/" = `echo $file | cut -c ${#file}-${#file}` ]; then
-    scp -i $rsaKey -r $host:$1 ./
+# upload
+if [ -f "$file" ]; then
+    scp -i $rsaKey $file $host:$remoteDir
+
+elif [ -d "$file" ]; then
+    scp -i $rsaKey -r $file $host:$remoteDir
+
 else
-    scp -i $rsaKey $host:$1 ./
+    echo "無効なファイルです"
+    exit 1
 fi
 

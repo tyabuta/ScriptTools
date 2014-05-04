d2992bee87604b33cf095e8b424d09a7a5987b98
tyabuta <gmforip@gmail.com>
2014-05-05 01:05:05 +0900

    git-checkout-branch.sh update

--- git-checkout-branch.sh
+++ git-checkout-branch.sh
@@ -10,8 +10,7 @@ curretBranch=$(git branch | awk '/^\* .+/ { print $2 }')
 [ -z "$curretBranch" ] && exit 1
 
 # ローカルブランチを取得
-branches=$(git branch --column | sed -e 's/\*//g')
-
+branches=$(git branch | awk '{buf=sprintf("%s %s ", buf, $0)} END{print buf}' | sed -e 's/\*//g')
 
 echo "切り替えるブランチを選んでください (on branch $(tput setaf 2)${curretBranch}$(tput sgr 0))"
 PS3='>>> '

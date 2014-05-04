138f3bc979f7ff43022560221da59ef90a00e976
tyabuta <gmforip@gmail.com>
2014-05-04 23:54:19 +0900

    git-checkout-branch.sh 追加

---
+++ git-checkout-branch.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+# -----------------------------------------------
+#
+#      branchの切り替えをlocalから選択する
+#
+# -----------------------------------------------
+
+# 現在のブランチ取得
+curretBranch=$(git branch | awk '/^\* .+/ { print $2 }')
+[ -z "$curretBranch" ] && exit 1
+
+# ローカルブランチを取得
+branches=$(git branch --column | sed -e 's/\*//g')
+
+echo "切り替えるブランチを選んでください (on branch ${curretBranch})"
+PS3='>>> '
+select branch in $branches; do
+    [ -z "$branch" ] && exit 0
+    break
+done
+
+# checkout
+git checkout $branch
+

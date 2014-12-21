19e1102765184644fcac28fa7c01f0f3cff58095
tyabuta <gmforip@gmail.com>
2014-12-21 21:51:21 +0900

    add homebrew-uninstall.sh

---
+++ homebrew-uninstall.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+# -----------------------------------------------
+# homebrew アンインストール用のスクリプト
+# -----------------------------------------------
+
+cd `brew --prefix`
+rm -rf Cellar/
+brew prune
+
+rm -rf .git
+rm -rf ~/Library/Caches/Homebrew
+rm -rf ~/Library/Logs/Homebrew
+rm -rf /Library/Caches/Homebrew
+rm -rf Library/Homebrew/
